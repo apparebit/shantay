1d7d5b95759a7e79a96af8fd078e61840aad06dc
Robert Grimm <apparebit@gmail.com>
2025-02-26 13:43:31 -0500

    fix type signature

--- shantay/render.py
+++ shantay/render.py
@@ -3,7 +3,7 @@ from IPython.display import HTML
 import polars as pl
 
 
-def to_html(df: pl.DataFrame) -> str:
+def to_html(df: pl.DataFrame) -> HTML:
     df = df.select(
         pl.all().exclude(
             "incompatible_content_explanation",
@@ -25,3 +25,23 @@ def to_html(df: pl.DataFrame) -> str:
         )
         .as_raw_html()
     )
+
+# def to_md_table(df: pl.DataFrame) -> Markdown:
+#     df = df.cast(str)
+#     widths = df.with_columns(
+#         pl.all().str.len_chars().max()
+#     ).row(0)
+#     colno = len(widths)
+
+#     def row(data: Sequence) -> str:
+#         assert len(data) == colno
+#         return f"|{'|'.join([f'{data[i] or "":<{widths[i]}}' for i in range(colno)])}|\n"
+
+#     def divider() -> str:
+#         return f"|{'|'.join(['-' * widths[i] for i in range(colno)])}|\n"
+
+#     return Markdown(
+#         row(df.columns)
+#         + divider()
+#         + "".join(row(df.row(i)) for i in range(df.height))
+#     )
