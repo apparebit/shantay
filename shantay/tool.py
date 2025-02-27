98d55e568e86e4ea0ace2de555a40637bf0ae52f
Robert Grimm <apparebit@gmail.com>
2025-02-27 13:32:37 -0500

    add support for logging to file

--- shantay/tool.py
+++ shantay/tool.py
@@ -1,12 +1,13 @@
 from argparse import ArgumentParser
 from dataclasses import dataclass
 import datetime as dt
+import logging
 from pathlib import Path
 import traceback
 
-from .metadata import Metadata
+from .metadata import Metadata, MetadataConflict
 from .progress import Progress
-from .release import Schedule
+from .release import DownloadFailed, Schedule
 from .sor import DailySoR
 from .runner import Task, Runner
 
@@ -19,40 +20,54 @@ class Options:
     start: dt.date
     stop: dt.date
     pipelines: int
+    logfile: Path
+    verbose: bool
 
 
 def _get_options(args: list[str]) -> Options:
     parser = ArgumentParser(prog="datascale")
     parser.add_argument(
-        "task",
-        choices=["prepare", "analyze"],
-        default="prepare",
-        help="select task to execute",
-    )
-    parser.add_argument(
         "--archive",
         type=Path,
-        help="set archive directory"
+        help="set the directory for storing downloaded archives"
     )
     parser.add_argument(
         "--batches",
         type=Path,
-        help="set batches directory"
+        help="set the directory for storing extracted category data"
     )
     parser.add_argument(
         "--start",
-        help="set start date"
+        help="set the start date"
     )
     parser.add_argument(
         "--stop",
-        help="set stop date (inclusive)",
+        help="set the stop date (inclusive)",
     )
     parser.add_argument(
         "--pipelines",
         default=1,
         type=int,
-        help="set the number of parallel pipelines"
+        help="set the number of parallel pipelines (currently ignored)"
     )
+    parser.add_argument(
+        "--logfile",
+        default="shantay.log",
+        type=Path,
+        help="set the file receiving log output"
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="enable verbose logging"
+    )
+    parser.add_argument(
+        "task",
+        choices=["prepare", "analyze"],
+        default="prepare",
+        help="select the task to execute",
+    )
+
     raw_options = parser.parse_args(args)
 
     return Options(
@@ -63,11 +78,20 @@ def _get_options(args: list[str]) -> Options:
             else dt.date(2023, 9, 25),
         stop=dt.date.fromisoformat(raw_options.stop) if raw_options.stop
             else dt.date.today() - dt.timedelta(days=2),
+        logfile=raw_options.logfile,
+        verbose=raw_options.verbose,
         pipelines=raw_options.pipelines if 1 <= raw_options.pipelines else 1
     )
 
 def _run(args: list[str]) -> None:
     options = _get_options(args)
+
+    logging.basicConfig(
+        filename=options.logfile,
+        encoding="utf8",
+        level=logging.DEBUG if options.verbose else logging.INFO,
+    )
+
     schedule = Schedule(DailySoR(options.start), DailySoR(options.stop))
 
     progress = Progress(row=None)
@@ -75,25 +99,33 @@ def _run(args: list[str]) -> None:
         archive=options.archive,
         batches=options.batches,
         progress=progress,
-        id=None,
     )
     staging_directories = [runner.staging]
 
     runner.prepare()
-    try:
-        for release in schedule.releases():
+    for release in schedule.releases():
+        try:
             if options.task is Task.PREPARE:
                 runner.prepare_batches(release)
             elif options.task is Task.ANALYZE:
                 runner.analyze_batches(release)
-    finally:
-        if options.task is Task.PREPARE:
-            Metadata.merge(staging_directories, options.batches)
+        except (DownloadFailed, MetadataConflict) as x:
+            raise
+        except Exception as x:
+            x.add_note(
+                f"WARNING: Artifacts for release {release} may be incomplete or corrupted!"
+            )
+            raise
+    if options.task is Task.PREPARE:
+        Metadata.merge(*staging_directories).write_json(options.batches)
 
 def run(args: list[str]) -> int:
     try:
         _run(args)
         return 0
+    except (DownloadFailed, MetadataConflict) as x:
+        print(str(x))
+        return 1
     except Exception as x:
         print("".join(traceback.format_exception(x)))
         return 1
