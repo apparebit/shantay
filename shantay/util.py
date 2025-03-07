cef91a1d1d4d0e6d637c61d316e8aefbd1b856d0
Robert Grimm <apparebit@gmail.com>
2025-03-06 19:57:09 -0500

    add Markdown table formatting

--- shantay/util.py
+++ shantay/util.py
@@ -53,3 +53,75 @@ def scale(value: float) -> tuple[float, str]:
         return value / 1_000_000, "mega"
     else:
         return value / 1_000_000_000, "giga"
+
+
+def to_markdown_table(rows: list[list[object]], column_names: list[str], title: str) -> str:
+    columns = [[it for it in column] for column in zip(*rows)]
+    if len(columns) == 0:
+        raise ValueError("no data columns to format")
+    if len(columns) != len(column_names):
+        raise ValueError(f"{len(columns)} columns but {len(column_names)} column names")
+
+    types = [_get_type(column) for column in columns]
+    columns = [
+        [fmt(it) for it in column]
+        for fmt, column in zip((_get_format(tp) for tp in types), columns)
+    ]
+    widths = [
+        max(len(name) + 6, *(len(it) + 2 for it in column))
+        for name, column in zip(column_names, columns)
+    ]
+
+    def format_row(data: Iterable[str]) -> str:
+        items = (
+            (f"{it:<{w}}" if tp is str else f"{it:>{w}}")
+            for it, w, tp in zip(data, widths, types)
+        )
+        return f'| {" | ".join(items)} |'
+
+    def format_div() -> str:
+        items = []
+        for width, tp in zip(widths, types):
+            before = ":" if tp is str else ""
+            dashes = "-" * (width - 3)
+            after = "" if tp is str else ":"
+            items.append(f" {before}{dashes}{after} ")
+        return f'| {" | ".join(items)} |'
+
+    return dedent(f"""\
+        __{title}__
+
+        {format_row(column_names)}
+        {format_div()}
+        {"\n".join(format_row(row) for row in zip(*columns))}
+    """)
+
+
+def _get_type(column: list[object]) -> type[int] | type[float] | type[str]:
+    tp = None
+    for cell in column:
+        if cell is None:
+            continue
+
+        ct = type(cell)
+        if tp is None and ct in (int, float):
+            tp = ct
+        elif tp is ct:
+            pass
+        elif tp is int and ct is float or tp is float and ct is int:
+            tp = float
+        else:
+            tp = str
+            break
+
+    assert tp is not None
+    return tp
+
+
+def _get_format(tp: type) -> Callable[[object], str]:
+    if tp is int:
+        return lambda c: f"{c:,}"
+    elif tp is float:
+        return lambda c: f"{c:.1f}"
+    else:
+        return lambda c: f"{c}"
