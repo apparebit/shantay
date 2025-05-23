bec9aa3d1b49d61f259b9f5cb24812e06abc6ad4
Robert Grimm <apparebit@gmail.com>
2025-05-23 02:11:24 -0400

    disable generation of data.txt helper file

--- test/test_prepare.py
+++ test/test_prepare.py
@@ -195,10 +195,10 @@ class TestPrepare(unittest.TestCase):
             frame = collector.frame()
             frame_data = frame.to_dict(as_series=False)
 
-            import pprint
+            # import pprint
 
-            with open("data.txt", mode="w", encoding="utf8") as file:
-                pprint.pprint(frame_data, stream=file)
+            # with open("data.txt", mode="w", encoding="utf8") as file:
+            #     pprint.pprint(frame_data, stream=file)
 
             self.assertEqual(frame_data, EXPECTED_ANALYSIS)
 
