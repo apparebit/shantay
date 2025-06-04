ee8245270335ec8573f3a8b56369ff17976661bb
Robert Grimm <apparebit@gmail.com>
2025-06-03 23:33:36 -0400

    update list of platforms and add a different method for getting the latest version

--- shantay/_platform.py
+++ shantay/_platform.py
@@ -10,22 +10,31 @@ import logging
 from pathlib import Path
 import re
 from types import MappingProxyType
+from urllib.request import Request, urlopen
+
 import polars as pl
 
 
 PlatformNames = (
+    "ADEO",
     "Adobe Lightroom",
     "Adobe Photoshop Express",
     "Adobe Stock",
+    "AGODA",
+    "Akciós-újság.hu",
     "AliExpress",
     "Amazon",
     "Amazon Store",
     "App Store",
     "Apple Books",
     "Apple Podcasts",
+    "Auctronia",
+    "AutoRevue",
+    "AutoScout24",
     "Azar",
     "Badoo",
     "Behance",
+    "BigBang.si",
     "BlaBlaCar",
     "bolha.com",
     "Booking.com",
@@ -36,62 +45,115 @@ PlatformNames = (
     "Cdiscount",
     "Chrome Web Store",
     "Conrad",
+    "Course Hero",
     "daft.ie",
     "Dailymotion",
+    "DATEV Marktplatz",
+    "DATEV SmartExperts",
     "Deliveroo Ireland",
+    "Delivery Hero",
     "Discord",
     "Doctolib",
     "DoneDeal.ie",
+    "e15",
+    "eBay",
+    "eJobs",
     "EMAG.BG",
     "EMAG.HU",
     "EMAG.RO",
     "eMimino.cz",
+    "Eventbrite",
     "Facebook",
     "Fashiondays.ro",
     "Flights",
+    "Flourish",
+    "G2.com",
+    "Garmin",
+    "Gastrojobs",
     "Glassdoor",
     "Google Maps",
     "Google Play",
     "Google Shopping",
+    "GroupMe",
+    "Groupon",
     "gutefrage.net",
     "Habbo",
+    "happn",
     "Használtautó.hu",
     "Hinge",
+    "Hornbach",
+    "Hostelworld.com",
     "Hotel Hideaway",
     "Hotels",
     "Hírstart",
+    "Idealista.com",
     "Idealo",
+    "IMDb",
     "imobiliare.ro",
     "Imovirtual",
     "ingatlan.com",
+    "Ingatlanbazár",
     "Instagram",
+    "irishjobs.ie",
+    "JetBrains",
+    "jobs.ie",
     "Joom",
     "Kaggle",
     "Kleinanzeigen",
+    "Knowunity",
+    "La Redoute",
     "leboncoin",
     "Ligaportal",
     "LinkedIn",
+    "ManoMano",
     "Meetic",
+    "Microsoft Operations",
+    "Microsoft Store",
     "Microsoft Teams",
+    "Milanuncios.com",
+    "Mimiaukce",
+    "Mindmegette",
     "mobile.de",
     "nebenan.de",
+    "Nebius AI",
+    "Njuškalo.hr",
+    "Nosalty",
     "OKCupid",
     "OLX",
+    "Other Meta Product",
     "OTTO",
+    "Parship",
+    "PC Games Store",
+    "Pexels",
+    "PHAISTOS NETWORKS",
     "Pinterest",
     "Plenty of Fish",
     "Pornhub",
+    "Profesia",
+    "profession.hu",
     "Pub.dev",
     "Quora",
     "Rajče",
     "Rakuten",
     "Reddit",
+    "rentalia.com",
+    "ResearchGate",
+    "rezeptwelt.de",
     "Roblox",
+    "Samsung PENUP",
+    "SAP",
+    "SE LOGER",
+    "SFDC",
     "Shein",
+    "Shopify",
+    "SME Blog",
     "Snapchat",
     "SoundCloud",
+    "Spark Networks",
     "Standvirtual",
+    "Startlap",
     "Stepstone",
+    "Streamate.com",
     "Stripchat",
     "Studydrive",
     "TAZZ",
@@ -99,6 +161,7 @@ PlatformNames = (
     "Temu",
     "Tenor",
     "The League",
+    "TheFork",
     "Threads",
     "TikTok",
     "Tinder",
@@ -109,6 +172,7 @@ PlatformNames = (
     "Udemy",
     "Vacation Rentals",
     "Vareni.cz",
+    "Vestiaire Collective",
     "Viator",
     "Vimeo",
     "Vinted",
@@ -117,29 +181,48 @@ PlatformNames = (
     "Waze",
     "WhatsApp",
     "willhaben",
+    "Wizz",
     "X",
+    "Xbox Store",
+    "Xbox.com",
     "YouTube",
+    "Yubo",
     "Zalando",
     "Živě.cz",
 )
 
 
 CanonicalPlatformNames = MappingProxyType({
+    "ADEO MARKETPLACE SERVICES": "ADEO",
     "Adobe Photoshop Lightroom": "Adobe Lightroom",
     "Apple Books (ebooks)": "Apple Books",
     "Apple Podcasts Subscriptions": "Apple Podcasts",
     "Discord Netherlands B.V.": "Discord",
+    "eDarling, EliteSingles, SilverSingles, Zoosk": "Spark Networks",
+    "foodora, Glovo, efood, foody": "Delivery Hero",
+    "Garmin Nederland B.V.": "Garmin",
+    "HORNBACH Marktplatz, Smart Home by HORNBACH": "Hornbach",
+    "Hostelworld.com Limited": "Hostelworld.com",
     "Meetic SAS": "Meetic",
+    "Microsoft Ireland Operations Limited": "Microsoft Operations",
+    "Microsoft Store on Windows (PC App Store)": "Microsoft Store",
     "Microsoft Teams personal": "Microsoft Teams",
+    "Other Meta Platforms Ireland Limited-offered Products": "Other Meta Product",
     "OTTO Market": "OTTO",
     "Quora Ireland Limited": "Quora",
+    "www.rentalia.com": "rentalia.com",
+    "SAP Community": "SAP",
+    "SFDC Ireland Limited": "SFDC",
     'SIA "JOOM"': "Joom",
+    "SIA &quot;JOOM&quot;": "Joom",
     "Vinted UAB": "Vinted",
     "WhatsApp Channels": "WhatsApp",
     "willhaben internet service GmbH & Co KG": "willhaben",
-    "www.gutefrage.net": "gutefrage.net"
+    "willhaben internet service GmbH &amp; Co KG": "willhaben",
+    "www.gutefrage.net": "gutefrage.net",
+    "Xbox Console Store": "Xbox Store",
+    "Xbox.com Website Store": "Xbox.com",
 })
-
 
 class MissingPlatformError(Exception):
     pass
@@ -206,9 +289,9 @@ def update_new_platform_names(names: Sequence[str]) -> bool:
         converge on the same final result.
      3. While concurrent updates may make names appear and disappear again, the
         restarted run of shantay will likely fail on re-encountering deleted
-        names again. On the off-chance that a name is deleted after it has
-        already been counted, the concatenation of summary statistics will fail
-        as it re-applies the statistics schema.
+        names. On the off-chance that a name is deleted after it has already
+        been counted, the concatenation of summary statistics will fail as it
+        re-applies the statistics schema.
 
     If concatenation fails, human intervention becomes necessary. It entails
     manually adding new platform names to this module. In other words, in the
@@ -217,6 +300,16 @@ def update_new_platform_names(names: Sequence[str]) -> bool:
     only option. In other words, the use of self-modifying code improves the
     user experience but does not obviate the need for maintaining an up-to-date
     list of platform names.
+
+    The above does assume that a complete list of platform names is required for
+    running Shantay. Without it, we couldn't declare an enum type for the
+    summary statistics' variant column. Since Pola.rs' categorical type
+    dynamically determines all members, we might consider that as an option. But
+    there is no good way for merging the categorical types of parallel
+    processes. At least, that was the case when I last tried. That leaves string
+    as a type, which would be rather wasteful since the universe of variant
+    column values is fairly small. Hence dealing with an every growing list of
+    platform names doesn't seem the worst option.
     """
     # Validate names.
     for name in names:
@@ -259,3 +352,49 @@ def update_new_platform_names(names: Sequence[str]) -> bool:
     tmp.write_text(f"{prefix}PlatformNames = (\n{s}\n){suffix}", encoding="utf8")
     tmp.replace(file)
     return True
+
+
+PAGE_PATTERN = re.compile(
+    r"""
+    <select[ ]name="platform_id\[\]"[ ]id="platform_id"[^>]*>
+        \s*
+        (
+            (?: (?: <option[^>]*>[^<]*</option>) \s* )*
+        )
+    </select>
+    """, re.VERBOSE
+)
+
+
+OPTION_PATTERN = re.compile(r'<option[^>]*>([^<]*)</option>')
+
+
+class DownloadFailed(Exception):
+    pass
+
+
+def download() -> list[str]:
+    url = "https://transparency.dsa.ec.europa.eu/statement"
+
+    with urlopen(Request(url, None, {})) as response:
+        if response.status != 200:
+            _logger.error(
+                'failed to download type="web page", status=%d, url="%s"',
+                response.status, url
+            )
+            raise DownloadFailed(
+                f'download of web page "{url}" failed with status {response.status}'
+            )
+
+        page = response.read().decode("utf8")
+
+    match = PAGE_PATTERN.search(page)
+    assert match is not None
+
+    platforms = OPTION_PATTERN.findall(match.group(1))
+    platforms = [CanonicalPlatformNames.get(p, p) for p in platforms]
+
+    for p in platforms:
+        print(f'"{p}",')
+
+    return platforms
