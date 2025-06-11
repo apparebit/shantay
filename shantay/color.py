74e736f8453e67bc653afb3072d47c9d90acb904
Robert Grimm <apparebit@gmail.com>
2025-06-11 10:14:35 -0400

    improve colors and remove redundant palette

--- shantay/color.py
+++ shantay/color.py
@@ -1,11 +1,11 @@
 """
 Shantay's color palette for charting categorical data.
 
-It is the same as [Observable's 2024 color
-palette](https://observablehq.com/blog/crafting-data-colors) with two more
-colors (dark purple and yellow green) added. The palette features fairly
-saturated and bright colors, hence facilitating charts that "pop." However, that
-can be a bit much at times, so manual curation still matters.
+It is based on [Observable's 2024 color
+palette](https://observablehq.com/blog/crafting-data-colors) with changes to the
+purple and brown as well as two more colors. Overally, colors are fairly
+saturated and bright, hence facilitating charts that "pop". However, that can be
+a bit much at times, so manual curation still matters.
 """
 BLUE = "#4269d0"
 ORANGE = "#efb118"
@@ -13,13 +13,13 @@ RED = "#ff725c"
 CYAN = "#6cc5b0"
 GREEN = "#3ca951"
 PINK = "#ff8ab7"
-PURPLE = "#a463f2"
+PURPLE = "#a365ef"
 LIGHT_BLUE = "#97bbf5"
-BROWN = "#9c6b4e"
+BROWN = "#a57356"
 GRAY = "#9498a0"
 
-DARK_PURPLE = "#a03d8e"
-YELLOW_GREEN = "#bad44a"
+MAGENTA = "#b457ae"
+OLIVE = "#bac83b"
 
 PALETTE = [
     BLUE,
@@ -32,12 +32,8 @@ PALETTE = [
     LIGHT_BLUE,
     BROWN,
     GRAY,
-    YELLOW_GREEN,
-    DARK_PURPLE,
-]
-
-KEYWORD_PALETTE = [
-    LIGHT_BLUE, BLUE, PURPLE, RED, ORANGE, GREEN, PINK, CYAN, BROWN, GRAY
+    MAGENTA,
+    OLIVE,
 ]
 
 
