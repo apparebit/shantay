bbb5e22a50e65a3e8a23538437aa46793a5541c8
Robert Grimm <apparebit@gmail.com>
2025-06-26 09:36:08 -0400

    remove most unused code

--- shantay/framing.py
+++ shantay/framing.py
@@ -69,7 +69,9 @@ NOT_NULL = NotNull()
 
 
 def predicate(
-    column: NoArgumentProvided | NotNull | None | str | Sequence[str] = NO_ARGUMENT_PROVIDED,
+    column: (
+        NoArgumentProvided | NotNull | None | str | Sequence[str]
+    ) = NO_ARGUMENT_PROVIDED,
     entity: NoArgumentProvided | NotNull | None | str = NO_ARGUMENT_PROVIDED,
     variant: NoArgumentProvided | NotNull | None | str = NO_ARGUMENT_PROVIDED,
     tag: NoArgumentProvided | NotNull | None | str = NO_ARGUMENT_PROVIDED,
@@ -149,44 +151,6 @@ def get_quantity(
     return frame.item() if frame.height == 1 else None
 
 
-def is_daily(frame: pl.DataFrame) -> bool:
-    """Test whether the statistics contain only daily data."""
-    return frame.select(
-        (pl.col("start_date") != pl.col("end_date")).len()
-    ).item() == 0
-
-
-def daily_groupies() -> list[pl.Expr]:
-    """
-    The Pola.rs expressions to `group_by` when computing daily statistics.
-    `is_daily()` must return `True` for the data frame being grouped.
-    """
-    return [
-        pl.col("start_date"),
-        pl.col("end_date"),
-        pl.col("tag"),
-        pl.col("platform"),
-        pl.col("column"),
-        pl.col("entity"),
-        pl.col("variant"),
-        pl.col("text"),
-    ]
-
-
-def monthly_groupies() -> list[pl.Expr]:
-    """The Pola.rs expressions to `group_by` when computing monthly statistics."""
-    return [
-        pl.col("start_date").dt.year().alias("year"),
-        pl.col("start_date").dt.month().alias("month"),
-        pl.col("tag"),
-        pl.col("platform"),
-        pl.col("column"),
-        pl.col("entity"),
-        pl.col("variant"),
-        pl.col("text"),
-    ]
-
-
 def aggregates() -> list[pl.Expr]:
     """The Pola.rs expressions to `agg` by when computing daily statistics."""
     return [
@@ -220,7 +184,9 @@ def finalize(frame: pl.DataFrame) -> pl.DataFrame:
     rechunks the data frame.
     """
     return frame.group_by(
-        *daily_groupies(), maintain_order=True
+        "start_date", "end_date", "tag", "platform",
+        "column", "entity", "variant", "text",
+        maintain_order=True
     ).agg(
         *aggregates()
     ).sort(
